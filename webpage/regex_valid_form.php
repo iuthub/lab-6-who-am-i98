c4641d24da3fa975fc4c1ad37ad8ac8ee447a5c6
Jasur 98 <jasurbekyoldoshev98@gmail.com>
2019-03-22 11:46:17 +0500

    first part done and regex_valid_form.php changed

--- webpage/regex_valid_form.php
+++ webpage/regex_valid_form.php
@@ -1,22 +1,20 @@
-<?php 
-
+<?php
 	$pattern="";
 	$text="";
 	$replaceText="";
-
+	$replacedText="";
 	$match="Not checked yet.";
-
 if ($_SERVER["REQUEST_METHOD"]=="POST") {
 	$pattern=$_POST["pattern"];
 	$text=$_POST["text"];
-
+	$replaceText=$_POST["replaceText"];
+	$replacedText=preg_replace($pattern, $replaceText, $text);
 	if(preg_match($pattern, $text)) {
-						$match="Match!";
+						$match="There is a match!";
 					} else {
-						$match="Does not match!";
+						$match="It does not match!";
 					}
 }
-
 ?>
 
 
@@ -27,24 +25,27 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
 	<title>Valid Form</title>
 </head>
 <body>
-	<form action="valid_form.php" method="post">
+	<form action="regex_valid_form.php" method="post">
 		<dl>
 			<dt>Pattern</dt>
 			<dd><input type="text" name="pattern" value="<?= $pattern ?>"></dd>
-			
+
 			<dt>Text</dt>
 			<dd><input type="text" name="text" value="<?= $text ?>"></dd>
-			
+
 			<dt>Replace Text</dt>
 			<dd><input type="text" name="replaceText" value="<?= $replaceText ?>"></dd>
 
 			<dt>Output Text</dt>
 			<dd><?=	$match ?></dd>
 
+			<dt>Replaced Text</dt>
+			<dd> <code><?=	$replacedText ?></code></dd>
+
 			<dt>&nbsp;</dt>
 			<dd><input type="submit" value="Check"></dd>
 		</dl>
-		
+
 	</form>
 </body>
 </html>
